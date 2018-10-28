256fc1be8b5a94044a8b8ebac692eb27f6a5dc7a
Dan Greer <dangreer97@gmail.com>
2018-10-27 21:30:57 -0400

    Cleaned up documentation.

--- src/FileHandler.java
+++ src/FileHandler.java
@@ -7,9 +7,20 @@ public class FileHandler {
 		// Never gets called
 	}
 
+	/**
+	 * Reads location's inventory file.
+	 * 
+	 * @author Dan Greer
+	 * @param locationName - String with the name of the location
+	 * @return a Map with the location's inventory
+	 */
 	public static Map<String, Integer> readFile(String locationName) {
+		
 		Map<String, Integer> inventory = new TreeMap<>();
+		
 		try {
+			
+			// Read all of the inventory from the location's inventory file
 			BufferedReader inputFile = new BufferedReader(new FileReader("data/" + locationName + ".txt"));
 			String line = inputFile.readLine();
 			while (line != null) {
@@ -23,12 +34,15 @@ public class FileHandler {
 			}
 			inputFile.close();
 		} catch (FileNotFoundException e) {
-			File newFile = new File(fileName);
+			
+			// If the file doesn't exist yet, create it
+			File newFile = new File(locationName + ".txt");
 			try {
 				newFile.createNewFile();
 			} catch (IOException e1) {
 				System.err.println("ERROR: File could not be created.");
 			}
+			
 		} catch (IOException e) {
 			System.err.println("ERROR: File input could not be read.");
 		}
@@ -37,32 +51,46 @@ public class FileHandler {
 		
 	}
 
+	/**
+	 * Writes the HTML index file for all of the locations.  The file will contain links to all of
+	 * the location's inventories.
+	 * 
+	 * @author Ally Chitwood
+	 * @param reliefLocations - List with all of the relief locations
+	 */
 	public static void writeHTML(List<Location> reliefLocations) {
 		// Write to an HTML table
 
 		try {
+			
+			// Write the top of the HTML file
 			BufferedWriter outputHTML = new BufferedWriter(new FileWriter("data/indexInventory.html"));
 			outputHTML.write("<!DOCTYPE html>");
 			outputHTML.newLine();
-			
 			outputHTML.write("<html lang=\"en\">");
 			outputHTML.newLine();
-			outputHTML.write("<link rel=\"stylesheet\" href=\"inventoryStyle.css\" type=\"text/css\"");
 			
-			// Head
+			// Write the header of the HTML file
 			outputHTML.write("\t<head>");
 			outputHTML.newLine();
 			outputHTML.write("\t\t<title> Disaster Relief Inventory</title>");
 			outputHTML.newLine();
+			outputHTML.write("\t\t<link rel=\"stylesheet\" href=\"inventoryStyle.css\" type=\"text/css\"");
+			outputHTML.newLine();
 			outputHTML.write("\t</head>");
 			outputHTML.newLine();
 			
-			// Body
+			// Write the body of the HTML file
 			outputHTML.write("\t<body>");
 			outputHTML.newLine();
-			outputHTML.write("\t<h1>Locations<h1>");
+			outputHTML.write("\t<h1>Disaster Relief Locations<h1>");
+			
+			// Write links to all of the locations files
 			for (Location loc: reliefLocations) {
-				outputHTML.write("\t\t<a href=\"data/" + loc.locationName + ".html>" + loc.locationName + "</a>");
+				outputHTML.write("\t\t<a href=\"" + loc.locationName + ".html\">" + loc.locationName + "</a>");
+				outputHTML.newLine();
+				outputHTML.write("\t\t<br/>");
+				outputHTML.newLine();
 			}
 			
 			outputHTML.write("\t</body>");
@@ -75,32 +103,47 @@ public class FileHandler {
 			
 			outputHTML.close();
 		} catch (IOException e) {
-			e.printStackTrace();
+			System.err.println("ERROR: Unable to write index HTML file.");
 		}
 			
 	}
 	
+	/**
+	 * Writes a specific location's inventory to it's HTML file.
+	 * 
+	 * @author Ally Chitwood
+	 * @param loc - Location whose inventory is being written
+	 */
 	public static void writeLocationHTML(Location loc) {
 		try {
+			
+			// Writes the top of the HTML file
 			BufferedWriter outputHTML = new BufferedWriter(new FileWriter("data/" + loc.locationName + ".html"));
 			outputHTML.write("<!DOCTYPE html>");
 			outputHTML.newLine();
-			
 			outputHTML.write("<html lang=\"en\">");
 			outputHTML.newLine();
-			outputHTML.write("<link rel=\"stylesheet\" href=\"inventoryStyle.css\" type=\"text/css\"");
 			
-			// Head
+			// Write the header of the HTML file
 			outputHTML.write("\t<head>");
 			outputHTML.newLine();
 			outputHTML.write("\t\t<title>" + loc.locationName + " Inventory</title>");
 			outputHTML.newLine();
+			outputHTML.write("\t\t<link rel=\"stylesheet\" href=\"inventoryStyle.css\" type=\"text/css\"");
+			outputHTML.newLine();
 			outputHTML.write("\t</head>");
 			outputHTML.newLine();
 			
-			// Body
+			// Write the body of the HTML file
 			outputHTML.write("\t<body>");
 			outputHTML.newLine();
+			
+			// Link to the index page
+			outputHTML.write("\t\t<a href=\"indexInventory.html\">Home</a>");
+			
+			// Print out the location name and all of it's items with their quantities
+			outputHTML.write("\t\t<h2>" + loc.getName() + " Inventory</h2>");
+			outputHTML.newLine();;
 			outputHTML.write("\t\t<table>");
 			outputHTML.newLine();
 			outputHTML.write("\t\t\t<tr>");
@@ -137,12 +180,21 @@ public class FileHandler {
 			
 			
 		} catch (IOException e) {
-			e.printStackTrace();
+			System.err.println("ERROR: Unable to write location HTML file.");
 		}
 	}
 
+	/**
+	 * Writes the location's inventory to a text file to be read from later.
+	 * 
+	 * @author Dan Greer
+	 * @param inventory - Map with the location's items and their quantities
+	 * @param locationName - location with the inventory
+	 */
 	public static void writeInventoryFile(Map<String, Integer> inventory, String locationName) {
 		try {
+			
+			// Write the full inventory to the locatin's text file
 			BufferedWriter outputFile = new BufferedWriter(new FileWriter("data/" + locationName + ".txt"));
 			
 			for (Map.Entry<String, Integer> entry: inventory.entrySet()) {
@@ -156,6 +208,55 @@ public class FileHandler {
 		} catch (IOException e) {
 			System.err.println("ERROR: File could not be written.");
 		}
+	}
+
+	/**
+	 * Writes the file containing a list of all the locations.
+	 * 
+	 * @author Dan Greer
+	 * @param reliefLocations - List of all the locations
+	 */
+	public static void writeLocationsFile(List<Location> reliefLocations) {
+		try {
+			
+			// Write all of the locations to the locations text file
+			BufferedWriter locationsWriter = new BufferedWriter(new FileWriter("data/locations.txt"));
+			for (Location loc : reliefLocations) {
+				locationsWriter.write(loc.getName());
+				locationsWriter.newLine();
+			}
+			
+			locationsWriter.close();
+		} catch (IOException e) {
+			System.err.println("ERROR: Could not write to locations.txt");
+		}
+		
+	}
+
+	/**
+	 * Reads from the locations text file and puts the locations into a List.
+	 * 
+	 * @author Dan Greer
+	 * @return a List containing all of the locations previously entered by the user
+	 */
+	public static List<Location> readLocationsFile() {
+		List<Location> reliefLocations = new ArrayList<>();
+		try {
+			
+			// Reads all of the lists from the locations text file and puts them all into a List
+			BufferedReader readLocations = new BufferedReader(new FileReader("data/locations.txt"));
+			String line = readLocations.readLine();
+			
+			while (line != null) {
+				reliefLocations.add(new Location(line));
+				line = readLocations.readLine();
+			}
+			
+			readLocations.close();
+		} catch (IOException e1) {
+			System.err.println("ERROR: Could not read locations.txt");
+		}
+		return reliefLocations;
 	}
 
 }
--- src/Location.java
+++ src/Location.java
@@ -4,16 +4,34 @@ public class Location {
 	String locationName;
 	Map<String, Integer> inventory;
 
+	/**
+	 * Constructor for Location.
+	 * 
+	 * @author Dan Greer
+	 * @param locationName - String with name of the location
+	 */
 	public Location(String locationName) {
 		this.locationName = locationName;
-		this.inventory = FileHandler.readFile("data/" + locationName + ".txt");
+		this.inventory = FileHandler.readFile(locationName);
 	}
 	
+	/**
+	 * Accessor for the location's name.
+	 * 
+	 * @author Dan Greer
+	 * @return the name of the location
+	 */
 	public String getName() {
 		return this.locationName;
 	}
 	
+	/**
+	 * Prints the inventory of the location.
+	 * 
+	 * @author Dan Greer
+	 */
 	public void printInventory() {
+		System.out.println("Inventory: ");
 		for (Map.Entry<String, Integer> entry: this.inventory.entrySet()) {
 			System.out.println(entry.getKey() + " - " + entry.getValue());
 		}
